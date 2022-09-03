a732fe6e6724cf0a679ae07189b88f3f9cf1e01a
johnowow <oao_john@yahoo.com>
2022-09-02 19:25:28 -0700

    Initial Commit

---
+++ src/main/java/edu/sdccd/cisc191/ArrayLab.java
@@ -0,0 +1,100 @@
+package edu.sdccd.cisc191;
+
+/**
+ * CISC191 2.3 Multidimensional Arrays - Junior
+ * @author John De Castro
+ */
+
+
+public class ArrayLab {
+    /**
+     * @param args command line parameters
+     */
+    public static void main(String[] args) {
+        int[][] spreadsheet = new int[][] { {1,2,3}, {4,5,6}, {7,8,9} };
+        System.out.println("Max: " + max(spreadsheet));
+        System.out.println("Sum of row 0: " + rowSum(spreadsheet,0));
+        System.out.println("Sum of col 0: " + columnSum(spreadsheet,0));
+        int[] rowSums = allRowSums(spreadsheet);
+        for(int row = 0; row < rowSums.length; row++) {
+            System.out.println("Sum of row " + row + ": " + rowSums[row]);
+        }
+
+    }
+
+    /**
+     * @param a 2D array
+     * @return the maximum value in the 2d parameter array a
+     */
+    public static int max(int[][] a)
+    {
+        int maxInt = a[0][0];
+        for ( int row = 0; row < a.length; row++)
+        {
+            for ( int column = 0; column < a[row].length; column++ )
+            {
+               if (a[row][column] > maxInt)
+               {
+                   maxInt = a[row][column];
+               }
+            }
+        }
+        return maxInt;
+    }
+
+    /**
+     * @param a 2D array
+     * @param x row index
+     * @return the sum of the elements in Row x of a
+     */
+    public static int rowSum(int[][] a, int x)
+    {
+        int sumOfRow = 0;
+        for ( int i = 0; i < a[x].length; i++ )
+        {
+            sumOfRow += a[x][i];
+        }
+        return sumOfRow;
+    }
+
+    /**
+     * @param a 2D array
+     * @param x column index
+     * @return the sum of the elements in Column x of a (careful with rows of different lengths!)
+     */
+    public static int columnSum(int[][] a, int x)
+    {
+        int sumOfColumn = 0;
+        for ( int row = 0; row < a.length; row++ )
+        {
+            if ( a[row].length <= x )
+            {
+                sumOfColumn += 0;
+            } else
+            {
+                sumOfColumn += a[row][x];
+            }
+        }
+        return sumOfColumn;
+    }
+
+    /**
+     * @param a 2D array
+     * @return calculates the row sum for every row and returns each of the values in an array. Index i of the return array contains the sum of elements in row i.
+     */
+    public static int[] allRowSums(int[][] a)
+    {
+        int[] sumOfEachRow = new int[a.length];
+        int sum;
+        for( int row = 0; row < a.length; row++)
+        {
+            sum = 0;
+            for( int column = 0; column < a[row].length; column++)
+            {
+                sum += a[row][column];
+            }
+            sumOfEachRow[row] = sum;
+        }
+        return sumOfEachRow;
+    }
+}
